7254f58f44b337c173cc73a25b49add27d295f1a
zahra noori sorkhani <zahranuori.1375@gmail.com>
2024-10-28 20:59:48 +0330

    first commit

---
+++ js/silder.js
@@ -0,0 +1,32 @@
+const $ = document;
+const sliderContainer = $.getElementById("slider");
+let sliderImage = $.querySelector(".slider-image");
+const srcImagesSlider = [
+    "./src/images/slider-1.jpg",
+    "./src/images/slider-2.jpg",
+    "./src/images/slider-3.jpg",
+    "./src/images/slider-4.jpg",
+    "./src/images/slider-5.jpg",
+    "./src/images/slider-6.jpg",
+    "./src/images/slider-7.jpg",
+    "./src/images/slider-8.jpg",
+    "./src/images/slider-9.jpg",
+    "./src/images/slider-10.jpg",
+];
+let counter = 0;
+window.addEventListener("load", ()=>{
+    console.log("ccc");
+    setInterval(()=>{
+      
+        if(counter < srcImagesSlider.length){
+            
+            sliderImage.setAttribute("src",srcImagesSlider[counter]);
+            console.log(srcImagesSlider[counter]);
+            counter++;
+       
+        }else if(counter === srcImagesSlider.length){
+            counter = 0;
+        }
+        console.log(counter);
+    },5000)
+})
